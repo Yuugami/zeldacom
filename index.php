af7511f4f22bb9da00cbfd4a875e0b567939468e
Windjy E. Jean <Yuugami@users.noreply.github.com>
2018-11-03 23:59:28 -0400

    Initial commit

---
+++ index.php
@@ -0,0 +1,115 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>The Legend of Zelda</title>
+    <link rel="stylesheet" type="text/css" href="css/style.css" />
+</head>
+
+<body>
+    <div class="container">
+        <div id="Ninty" class="horiBar">
+            <img src="img/nintendo-logo.png" alt="Nintendo Icon Bold">
+        </div>
+        <div id="navbar" class="horiBar">
+            <div id="NintyLogo"></div>
+            <ul>
+                <li><a href="#">Wii U</a></li>
+                <li><a href="#">Nintendo 3DS</a></li>
+                <li><a href="#">amiibo</a></li>
+                <li><a href="#">Games</a></li>
+                <li><a href="#">Play Nintendo</a></li>
+                <li><a href="#">Support</a></li>
+                <li><a href="#">My Nintendo</a></li>
+            </ul>
+        </div>
+        <div id="slideshow">
+            <div id="ssTop" class="horiBar">
+                <div id="title">
+                    <img src="img/headline-left.png" alt="Left Arrow">
+                    <img src="img/official-site-of.png" alt="Official Site">
+                    <img src="img/the-legend-of-zelda-series.png" alt="Zelda Series">
+                    <img src="img/headline-right.png" alt="Right Arrow">
+                </div>
+            </div>
+            <div id="ssMid" class="horiBar">
+
+            </div>
+            <div id="ssBot" class="horiBar">
+
+            </div>
+        </div>
+        <div id="gallery">
+            <div id="WiiU">
+                <div class="consoleName">
+                    <img src="img/keyline-3-left.png" alt="Arrow Left">
+                    <img src="img/wiiu.png" alt="Wii U">
+                    <img src="img/keyline-3-right.png" alt="Arrow Right">
+                </div>
+                <div class="gameContainer">
+                    <div class="game"><img src="img/the-legend-of-zelda-breath-of-the-wild.jpg" alt="Breath of the Wild"></div>
+                    <div class="game"><img src="img/twilightprincesshd.png" alt="Twilight Princess HD"></div>
+                    <div class="game"><img src="img/windwakerhd.png" alt="The Windwaker HD"></div>
+                    <div class="game"><img src="img/hyrulewarriors.png" alt="Hyrule Warriors"></div>
+                    <div class="game"><img src="img/minishcap.png" alt="Minish Cap"></div>
+                    <div class="game"><img src="img/linkstothepast.png" alt="Link to the Past"></div>
+                    <div class="game"><img src="img/adventureoflink.png" alt="Adventure of Link"></div>
+                    <div class="game"><img src="img/neszelda.png" alt="The Legend of Zelda"></div>
+                </div>
+            </div>
+            <div id="3DS">
+                <div class="consoleName">
+                    <img src="img/keyline-3-left.png" alt="Arrow Left">
+                    <img src="img/3ds.png" alt="3DS">
+                    <img src="img/keyline-3-right.png" alt="Arrow Right">
+                </div>
+                <div class="gameContainer">
+                    <div class="game"><img src="img/hyrulelegends.png" alt="Hyrule Warrior Legends"></div>
+                    <div class="game"><img src="img/triforceheroes.png" alt="Triforce Heroes"></div>
+                    <div class="game"><img src="img/majorasmask3d.png" alt="Majora's Mask 3D"></div>
+                    <div class="game"><img src="img/linkbetweenworlds.png" alt="A Link Between Worlds"></div>
+                    <div class="game"><img src="img/oracleofseasons.png" alt="Oracle of Seasons"></div>
+                    <div class="game"><img src="img/oracleofages.png" alt="Oracle of Ages"></div>
+                    <div class="game"><img src="img/linksawakeningdx.png" alt="Link's Awakening"></div>
+                    <div class="game"><img src="img/ocarinaoftime3d.png" alt="Ocarina of Time 3D"></div>
+                    <div class="game"><img src="img/spirittracks.png" alt="Spirit Tracks"></div>
+                    <div class="game"><img src="img/phantomhourglass.png" alt="Phantom Hourglass"></div>
+                </div>
+            </div>
+            <div id="Wii">
+                <div class="consoleName">
+                    <img src="img/keyline-3-left.png" alt="Arrow Left">
+                    <img src="img/wii.png" alt="Wii">
+                    <img src="img/keyline-3-right.png" alt="Arrow Right">
+                </div>
+                <div class="gameContainer">
+                    <div class="game"><img src="img/skywardsword.png" alt="Skyward Sword"></div>
+                    <div class="game"><img src="img/linkscrossbowtraining.png" alt="Crossbow Training"></div>
+                    <div class="game"><img src="img/twilightprincess.png" alt="Twilight Princess"></div>
+                    <div class="game"><img src="img/majorasmask.png" alt="Majora's Mask"></div>
+                </div>
+            </div>
+        </div>
+        <div class="horiBar"></div>
+        <div id="footer">
+            <div id="logos">
+                <img src="img/logo_nintendo_light.png" alt="NintendoLogo">
+                <div id="socialicons">
+                    <p>Follow Us:</p>
+                    <img src="img/follow_facebook_solo.png" alt="FacebookIcon">
+                    <img src="img/follow_twitter_solo.png" alt="TwitterIcon">
+                    <img src="img/follow_youtube_solo.png" alt="YouTubeIcon">
+                    <p> Privacy Notice </p>
+                    <p> Terms Of Notice </p>
+                </div>
+                <img src="img/e_t_esrb.gif" alt="RatingIcon">
+                <img src="img/esrb_privacy.jpg" alt="ESRBIcon">
+            </div>
+            <p>© 1986 - 2016 Nintendo. Nintendo properties are trademarks of Nintendo.</p>
+            <p><a href="#">Interest-Based Ads</a></p>
+        </div>
+    </div>
+</body>
+
+</html>
